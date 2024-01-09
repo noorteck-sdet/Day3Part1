c9b74c94a36c4e798d0ce27860477b8a846bd62c
AhmedAlanAly <155407231+AhmedAlanAly@users.noreply.github.com>
2024-01-08 18:16:03 -0600

    Update Demo1.java

--- src/team1/Demo1.java
+++ src/team1/Demo1.java
@@ -1,16 +1,6 @@
 package team1;
 
 public class Demo1 {
-
-	public static void main(String[] args) {
-		System.out.println("Team 1");
-		
-		System.out.println("Hello from Team4 ;)");
-
-		//random
-		
-		System.out.println("Where is the rest of Team 1?");
-		
-		// this is Freshta's line of code
+// Team 1 isa not active, team 3 is the best
 	}
 }
