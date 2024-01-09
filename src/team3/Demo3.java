fbe2e4003380b4b1a6390e017b3ddae79c840618
aaldu <aaldu@DESKTOP-26OB3J1.myfiosgateway.com>
2024-01-08 20:26:17 -0500

    yes

--- src/team3/Demo3.java
+++ src/team3/Demo3.java
@@ -7,14 +7,14 @@ public class Demo3 {
 		System.out.println("team 3 is the best");
 		System.out.println("Kamy");
 
-	}
+	
 
  System.out.println("Hello from Team4 ;)");
  System.out.println("HACKED BY TEAM 2");
  System.out.println("HACKED BY TEAM 2");
  System.out.println("HACKED BY TEAM 2");
  System.out.println("HACKED BY TEAM 2");
- 
+	}
 
  //Jackie Here
 }
--- src/team4/Demo4.java
+++ src/team4/Demo4.java
@@ -1,6 +1,10 @@
 package team4;
 
 public class Demo4 {
+	
+	public static void main(String[] args) {
+		
+	
 
 	 	System.out.println("Java is so much fun with team 3");
 		
@@ -13,8 +17,9 @@ public class Demo4 {
 
 	
 	//method by Ivan
-	public static void printInteger(Integer... i) {
-
+	public static void printInteger(Integer i) {
+		
+		Integer[] d = null;
 		for (Integer integer : d) {
 
 			System.out.println(integer);
